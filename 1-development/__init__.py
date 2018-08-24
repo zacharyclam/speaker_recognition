0c2f9d93c98395bb89f72e210fec5594d6681ef6
zacharyclam <25079094+zacharyclam@users.noreply.github.com>
2018-08-24 18:09:26 +0800

    seperate train file

---
+++ 1-development/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/env/python python3
+# -*- coding: utf-8 -*-
+# @File     : __init__.py
+# @Time     : 2018/8/24 17:51 
+# @Software : PyCharm
--- 1-development/train.py
+++ 1-development/train.py
@@ -8,12 +8,13 @@ import os
 import tensorflow as tf
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
-from keras.utils import to_categorical
 import keras.backend as K
-
 import numpy as np
 
-from .model import construct_model
+from model import construct_model
+from data_feeder import generate_fit
+
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
 
 tf.flags.DEFINE_integer(
     "batch_size", default=32,
@@ -36,15 +37,15 @@ tf.flags.DEFINE_string(
     help="the category of data")
 
 tf.flags.DEFINE_string(
-    "model_dir", default="model",
+    "model_dir", default=os.path.join(parent_dir, "model"),
     help="the model file dir")
 
 tf.flags.DEFINE_string(
-    "tensorboard_dir", default="logs",
+    "tensorboard_dir", default=os.path.join(parent_dir, "logs"),
     help="the tensorboard file dir")
 
 tf.flags.DEFINE_string(
-    "datalist_dir", default="data/bin",
+    "datalist_dir", default=os.path.join(parent_dir, "data/bin"),
     help="the data list file dir")
 
 # FLAGS 是一个对象，保存了解析后的命令行参数
@@ -52,27 +53,17 @@ FLAGS = tf.flags.FLAGS
 # 进行解析
 FLAGS.flag_values_dict()
 
-# set parameters
-batchSize = FLAGS.batch_size
-nClasses = FLAGS.num_classes
-nEpochs = FLAGS.num_epochs
-learnRate = FLAGS.learn_rate
 
-dataListDir = FLAGS.datalist_dir
-category = FLAGS.category
-modelDir = FLAGS.model_dir
-tensorboardDir = FLAGS.tensorboard_dir
+if not os.path.exists(FLAGS.model_dir):
+    os.makedirs(FLAGS.model_dir)
 
-if not os.path.exists(modelDir):
-    os.makedirs(modelDir)
-
-if not os.path.exists(tensorboardDir):
-    os.makedirs(tensorboardDir)
+if not os.path.exists(FLAGS.tensorboard_dir):
+    os.makedirs(FLAGS.tensorboard_dir)
 
 # the paths
 cwd = os.getcwd()  # current working directory
-train_path = os.path.join(dataListDir, "train_list.txt")  # train text path
-test_path = os.path.join(dataListDir, "test_list.txt")
+train_path = os.path.join(FLAGS.datalist_dir, "train_list.txt")  # train text path
+test_path = os.path.join(FLAGS.datalist_dir, "test_list.txt")
 
 # count the number of samples
 f = open(train_path)
@@ -82,40 +73,6 @@ f.close()
 f = open(train_path)
 nTest = len(f.readlines())  # number of train samples
 f.close()
-
-
-# 生成数据
-def generate_fit(path, batch_size, classe_nums):
-    """
-
-    :param path: 数据路径
-    :param batch_size:
-    :param classe_nums: 类别
-    :return:
-    """
-    with open(path) as f:
-        data_list = [line.strip().split(' ') for line in f]
-    index = np.arange(len(data_list))
-    while True:
-        cnt = 0
-        X = []
-        Y = []
-        # shffle data
-        np.random.shuffle(index)
-        data_list = np.array(data_list)[index, :]
-        for bin_path, label in data_list:
-            x = np.fromfile(bin_path, dtype=np.float)
-            X.append(x)
-            Y.append(label)
-
-            cnt += 1
-
-            if cnt % batch_size == 0:
-                yield [np.array(X)[:, :, np.newaxis], np.array(to_categorical(Y, classe_nums))]
-                X = []
-                Y = []
-                cnt = 0
-
 
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = "1"
@@ -128,30 +85,31 @@ if __name__ == '__main__':
     config.gpu_options.per_process_gpu_memory_fraction = 0.45  # 占用GPU90%的显存
     K.set_session(tf.Session(config=config))
 
-    extract_feature_model, sr_model = construct_model(nClasses)
+    # 创建模型
+    extract_feature_model, sr_model = construct_model(FLAGS.num_classes)
 
-    opt = Adam(lr=learnRate, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    opt = Adam(lr=FLAGS.learn_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     sr_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10,
                                   min_lr=1e-7, mode="min", cooldown=5)
 
-    tbCallBack = TensorBoard(log_dir=tensorboardDir,
+    tbCallBack = TensorBoard(log_dir=FLAGS.tensorboard_dir,
                              histogram_freq=0,
                              write_graph=True,
                              write_images=True)
 
-    checkpoint = ModelCheckpoint(filepath=os.path.join(modelDir, "checkpoint-{epoch:05d}-{acc:.2f}.h5"),
+    checkpoint = ModelCheckpoint(filepath=os.path.join(FLAGS.model_dir, "checkpoint-{epoch:05d}-{acc:.2f}.h5"),
                                  monitor='val_acc', verbose=2, save_best_only=True, mode='max')
 
-    sr_model.fit_generator(generate_fit(train_path, batchSize, nClasses),
-                          steps_per_epoch=np.ceil(nTrain / batchSize),
-                          shuffle=True,
-                          validation_data=generate_fit(test_path, batchSize, nClasses),
-                          validation_steps=np.ceil(nTest / batchSize),
-                          epochs=nEpochs,
-                          verbose=2,
-                          callbacks=[reduce_lr, checkpoint, tbCallBack]
-                          )
+    sr_model.fit_generator(generate_fit(train_path, FLAGS.batch_size, FLAGS.num_classes),
+                           steps_per_epoch=np.ceil(nTrain / FLAGS.batch_size),
+                           shuffle=True,
+                           validation_data=generate_fit(test_path, FLAGS.batch_size, FLAGS.num_classes),
+                           validation_steps=np.ceil(nTest / FLAGS.batch_size),
+                           epochs=FLAGS.num_epochs,
+                           verbose=2,
+                           callbacks=[reduce_lr, checkpoint, tbCallBack]
+                           )
 
-    extractFeatureModel.save("spk.h5")
+    extract_feature_model.save("spk.h5")
